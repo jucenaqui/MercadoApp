53b2246e364559806779ceb3da5cbd2b02addd7c
julio.naranjo@ofi.com.co <you@example.com>
2021-06-11 09:57:52 -0500

    update docs to publish

--- src/components/header/HeaderBarApp.js
+++ src/components/header/HeaderBarApp.js
@@ -3,12 +3,13 @@ import React from 'react';
 import SearchApp from '../search/SearchApp';
 
 import './HeaderBarApp.scss';
+// import logo from 'logo-header.png';
 
 export const HeaderBarApp = ({history}) => {
     return (
         <div className="header-bar">
             <div className="container-search">
-                <img src={ process.env.PUBLIC_URL + './public/logo-header.png' } alt="logo" />
+                <img src={ 'logo-header.png' } alt="logo" />
                 <SearchApp history={ history } />
             </div>
         </div>
