7009d93384a07bd2a29e8cda242c09023fa7c240
julio.naranjo@ofi.com.co <you@example.com>
2021-06-11 12:29:55 -0500

    pages9

--- src/components/header/HeaderBarApp.js
+++ src/components/header/HeaderBarApp.js
@@ -3,9 +3,10 @@ import React from 'react';
 import SearchApp from '../search/SearchApp';
 
 import './HeaderBarApp.scss';
-import logo from '../../assets/logo-header.png';
+// import logo from '../../assets/logo-header.png';
 
 export const HeaderBarApp = ({history}) => {
+    const logo = "https://firebasestorage.googleapis.com/v0/b/mercadolibreprueba.appspot.com/o/logo-header.png?alt=media&token=05b1170a-abb5-4159-ad6b-ae3f130392c5";
     return (
         <div className="header-bar">
             <div className="container-search">
